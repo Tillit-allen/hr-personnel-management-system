35d4dfd5c94efe75b7429c27e41b0bb6fa91de7d
lihang <1106224439@qq.com>
2019-01-02 14:29:10 +0800

    jsp页面初始化 解决了静态资源全部拦截的问题

---
+++ src/main/java/com/six/hrpms/pojo/Login_po.java
@@ -0,0 +1,22 @@
+package com.six.hrpms.pojo;
+
+public class Login_po {
+    String loginName;
+    String password;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
